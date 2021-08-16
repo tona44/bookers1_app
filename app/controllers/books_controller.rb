0168d1372f16be2fbe5a334a57adad48ced84a6e
tona44 <kuryu.waku@gmail.com>
2021-08-16 15:49:32 +0000

    [add] 新規投稿機能

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,30 @@
+class BooksController < ApplicationController
+  
+  def index
+    @book = Book.new
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
+    
+  end
+
+  def edit
+  end
+
+
+  private
+  def book_params
+    params.require(:book).permit(:title,:body)
+  end
+
+
+end
