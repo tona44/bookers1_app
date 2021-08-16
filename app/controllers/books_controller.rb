b787bbd1c511ebfc9509029632a6281767728320
tona44 <kuryu.waku@gmail.com>
2021-08-16 18:45:37 +0000

    [add] create機能・閲覧機能・編集機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,19 +6,17 @@ class BooksController < ApplicationController
   end
 
   def show
-  end
-
-  def new
+    @book = Book.find(params[:id])
   end
 
   def create
     book = Book.new(book_params)
     book.save
     redirect_to book_path(book.id)
-    
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
 
