32be7c06a64bf5b40120b67349021b1ab0130dfc
tona44 <kuryu.waku@gmail.com>
2021-08-17 07:12:56 +0000

    [add] 削除機能・フラッシュ機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,5 @@
 class BooksController < ApplicationController
-  
+
   def index
     @book = Book.new
     @books = Book.all
@@ -12,12 +12,25 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to book_path(book.id)
+    redirect_to book_path(book.id),notice:"Book was successfully created."
   end
 
   def edit
     @book = Book.find(params[:id])
   end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book),notice:"Book was successfully updated."
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path,notice:"Book was successfully destroyed."
+  end
+
 
 
   private
